1c33a30a7cfe9687c6c4c356c9099b511b0711d1
Zsvob <svobodazachary@gmail.com>
2022-02-17 17:39:38 -0500

    Category and Product Models

--- models/Category.js
+++ models/Category.js
@@ -2,11 +2,25 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
-    // define columns
+    id: {
+      // use the special Sequelize DataTypes object provide what type of data it is
+      type: DataTypes.INTEGER,
+      // this is the equivalent of SQL's `NOT NULL` option
+      allowNull: false,
+      // instruct that this is the Primary Key
+      primaryKey: true,
+      // turn on auto increment
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+
+    },
   },
   {
     sequelize,
@@ -14,7 +28,8 @@ Category.init(
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
-  }
+  },
+  
 );
 
 module.exports = Category;
