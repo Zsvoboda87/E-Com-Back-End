769f012b271196268de7067abd3b04c3fbd68836
Zsvob <svobodazachary@gmail.com>
2022-02-23 10:54:31 -0500

    tag Routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -13,7 +13,7 @@ router.get("/", (req, res) => {
       },
     ],
   })
-    .then((dbPostData) => res.json(dbPostData))
+    .then((Data) => res.json(Data))
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
@@ -33,7 +33,7 @@ router.get("/:id", (req, res) => {
       id: req.params.id,
     },
   })
-    .then((dbPostData) => res.json(dbPostData))
+    .then((Data) => res.json(Data))
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
@@ -42,14 +42,48 @@ router.get("/:id", (req, res) => {
 
 router.post("/", (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then((Data) => res.json(Data))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+    Tag.update(
+    {
+      // All the fields you can update and the data attached to the request body.
+      tag_name: req.body.tag_name,
+    },
+    {
+      // Gets a book based on the book_id given in the request parameters
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updated) => {
+      res.json(updated);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedCat) => {
+      res.json(deletedCat);
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
